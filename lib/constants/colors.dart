1ecf76eedb491d3199aa9cbe3d1d2779b907a588
Willy <kadekwilly006@gamil.com>
2025-06-25 00:05:05 +0800

    nambahin color active dan anactive

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFF976844);
 const Color secondaryColor = Color(0xFFC0721F);
 const Color backgroundColor = Color(0xFFF5EFE7);
+const Color anActiveItems = Color(0xFFB4B4B4);
 const Color inputBorderColor = Color(0xFF976844);
 const Color buttonColor = Color(0xFFC0721F);
 const Color textWhite = Color(0xffffffff);
+const Color activeItems = Color(0xFFD18629);
